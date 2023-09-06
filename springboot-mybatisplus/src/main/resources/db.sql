6c565f02207f8a9a9c98ff41807fc98852d1763e
czs <760620329@qq.com>
2023-09-06 21:08:18 +0800

    feat：完善springboot整合mybatis-plus使用的数据库脚本

--- springboot-mybatisplus/src/main/resources/db.sql
+++ springboot-mybatisplus/src/main/resources/db.sql
@@ -1,45 +1,76 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 127.0.0.1
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 50730
+ Source Server Version : 50743
  Source Host           : localhost:3306
- Source Schema         : mybatisplus
+ Source Schema         : realtime
 
  Target Server Type    : MySQL
- Target Server Version : 50730
+ Target Server Version : 50743
  File Encoding         : 65001
 
- Date: 08/05/2021 17:54:19
+ Date: 06/09/2023 21:05:31
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for product
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product`  (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `price` decimal(10, 2) NULL DEFAULT NULL,
+  `version` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of product
+-- ----------------------------
+INSERT INTO `product` VALUES (1, '笔记本', 120.00, 2);
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-                        `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-                        `name` varchar(30) DEFAULT NULL COMMENT '姓名',
-                        `age` int(11) DEFAULT NULL COMMENT '年龄',
-                        `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
-                        `create_time` datetime DEFAULT NULL,
-                        `update_time` datetime DEFAULT NULL,
-                        `version` int(255) DEFAULT NULL,
-                        `deleted` int(255) DEFAULT NULL,
-                        PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+CREATE TABLE `user`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `age` int(11) NULL DEFAULT NULL COMMENT '年龄',
+  `sex` tinyint(4) NULL DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `create_time` datetime NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
+  `version` int(255) NULL DEFAULT NULL,
+  `deleted` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'czs', 18, '7606@qq.com', '2021-05-08 16:56:09', '2021-05-08 17:12:58', 0, 1);
-INSERT INTO `user` VALUES (2, 'Jack', 20, 'test2@baomidou.com', '2021-05-08 16:56:12', '2021-05-08 16:56:25', NULL, NULL);
-INSERT INTO `user` VALUES (3, 'Tom', 28, 'test3@baomidou.com', '2021-05-08 16:56:14', '2021-05-08 16:56:27', NULL, NULL);
-INSERT INTO `user` VALUES (4, 'Sandy', 21, 'test4@baomidou.com', '2021-05-08 16:56:16', '2021-05-08 16:56:29', NULL, NULL);
-INSERT INTO `user` VALUES (5, 'Billie', 24, 'test5@baomidou.com', '2021-05-08 16:56:20', '2021-05-08 16:56:32', NULL, NULL);
+INSERT INTO `user` VALUES (2, 'czs', 18, 1, '123@qq.com', NULL, NULL, NULL, 0);
+INSERT INTO `user` VALUES (3, 'Enum', 20, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `user` VALUES (4, 'Enum', 20, 2, NULL, NULL, NULL, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for user_cdc
+-- ----------------------------
+DROP TABLE IF EXISTS `user_cdc`;
+CREATE TABLE `user_cdc`  (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `age` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_cdc
+-- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
